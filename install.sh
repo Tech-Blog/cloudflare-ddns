8f4b8b1088a091c8f12aacd34331580dc8dfe18e
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2018-08-30 23:46:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ echo Enter your cloudflare account email
 read -p 'Email Address: ' usermail
 echo Enter your cloudflare Api Key
 read -p 'Cloudflare Api: ' userapi
-echo Enter your Domain Name (domain.com etc)
+echo Enter your Domain Name 
 read -p 'Domain Name: ' userdomain
-echo Enter records to update (without the domain, for example: for a.domain.com press only a).
-echo you can use multiple records like this: 'a','b','c'
+echo Enter records to update
 read -p 'records: ' userrecords
 echo Thankyou $uservar we now have your login details $passvar
 
