82fea92a33f3dcd43fe74574175e7f0e84ca13d3
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2018-08-30 23:45:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 sudo apt update
-sudo apt upgrade --yes
 apt install python-pip --yes
 pip install pycfdns
-if [ ! -d "/opt/ddns" ]; then
-  mkdir /opt/ddns
-fi
-wget https://raw.githubusercontent.com/Tech-Blog/cloudflare-ddns/master/ddns.py -O /opt/ddns/ddns.py
-wget https://raw.githubusercontent.com/Tech-Blog/cloudflare-ddns/master/settings.sh -O /tmp/settings.sh
-bash /tmp/settings.sh
+
+echo Enter your cloudflare account email
+read -p 'Email Address: ' usermail
+echo Enter your cloudflare Api Key
+read -p 'Cloudflare Api: ' userapi
+echo Enter your Domain Name (domain.com etc)
+read -p 'Domain Name: ' userdomain
+echo Enter records to update (without the domain, for example: for a.domain.com press only a).
+echo you can use multiple records like this: 'a','b','c'
+read -p 'records: ' userrecords
+echo Thankyou $uservar we now have your login details $passvar
+
+sed -i -e 's/[token]/$userapi/g' /opt/ddns/ddns.py
+sed -i -e 's/[email]/$usermail/g' /opt/ddns/ddns.py
+sed -i -e 's/[zone]/$userdomain/g' /opt/ddns/ddns.py
+sed -i -e 's/[records]/[$userrecords]/g' /opt/ddns/ddns.py
+rm install.sh
