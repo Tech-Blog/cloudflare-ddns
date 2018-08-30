3c0d3040d6d7dc560dc51d028b02cf7909f59345
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2018-08-30 23:38:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ if [ ! -d "/opt/ddns" ]; then
   mkdir /opt/ddns
 fi
 wget https://raw.githubusercontent.com/Tech-Blog/cloudflare-ddns/master/ddns.py -O /opt/ddns/ddns.py
+wget https://raw.githubusercontent.com/Tech-Blog/cloudflare-ddns/master/settings.sh -O /tmp/settings.sh
+bash /tmp/settings.sh
