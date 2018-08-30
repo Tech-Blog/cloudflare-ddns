e8bd43ff6cd24356eff10636cfcc633cb4afd65f
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2018-08-31 00:20:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ echo Enter records to update
 read -p 'records: ' userrecords
 echo Thankyou $uservar we now have your login details $passvar
 
-while read a ; do echo ${a//token/$userapi} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
-while read a ; do echo ${a//email/$usermail} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
-while read a ; do echo ${a//zone/$userdomain} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
-while read a ; do echo ${a//subdomains/$userrecords} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
+while read a ; do echo ${a//_token/$userapi} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
+while read a ; do echo ${a//_email/$usermail} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
+while read a ; do echo ${a//_zone/$userdomain} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
+while read a ; do echo ${a//_subdomains/$userrecords} ; done < /opt/ddns/ddns.py > /opt/ddns/ddns.py.t ; mv /opt/ddns/ddns.py{.t,}
 
 rm install.sh
