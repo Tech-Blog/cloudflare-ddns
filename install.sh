91ce0b0803ddc0709bd91aff6d8a5c0e2ae65cde
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2018-08-30 15:47:55 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt upgrade --yes
+apt install python-pip --yes
+pip install pycfdns
+if [ ! -d "/opt/ddns" ]; then
+  mkdir /opt/ddns
+fi
+wget https://raw.githubusercontent.com/Tech-Blog/cloudflare-ddns/master/ddns.py -O /opt/ddns/ddns.py
